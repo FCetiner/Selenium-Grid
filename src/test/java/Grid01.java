10fa6d708daca62b43eddc321e1acf72b9090759
Ferhat <fcetinerr@gmail.com>
2022-02-23 00:59:33 +0300

    first commit

---
+++ src/test/java/Grid01.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Driver;
+
+public class Grid01 {
+
+    public static void main(String[] args) throws MalformedURLException {
+        WebDriver driver=new RemoteWebDriver(new URL("http://192.168.2.58:4444"),new ChromeOptions());
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
+        driver.close();
+    }
+}
